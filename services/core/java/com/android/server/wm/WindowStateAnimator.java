c734f7235b558cf576e760dda322cad827c4fd22
Phil Weaver <pweaver@google.com>
2017-07-12 14:04:16 -0700

    Back-port fixes for b/62196835
    Bug: 62196835
    Test: Created an accessibility service that displays a system
    and a toast overlay, confirmed that it disappeared when we
    reached the accessibility permission screen that uses this
    flag.
    
    Change-Id: Ic51ead670fc480e549512ba1d02f49d9c13bc3f0
    (cherry picked from commit 41ff5389daa6e6ce4aa853bfae96e5ced0b1d8df)
    CVE-2017-0752

--- services/core/java/com/android/server/wm/WindowStateAnimator.java
+++ services/core/java/com/android/server/wm/WindowStateAnimator.java
@@ -511,6 +511,7 @@ class WindowStateAnimator {
             if (WindowManagerService.SHOW_TRANSACTIONS) WindowManagerService.logSurface(mWin,
                     "HIDE (performLayout)", null);
             if (mSurfaceControl != null) {
+                mService.updateNonSystemOverlayWindowsVisibilityIfNeeded(mWin, false);                
                 mSurfaceShown = false;
                 try {
                     mSurfaceControl.hide();
@@ -1849,6 +1850,8 @@ class WindowStateAnimator {
                 if (mSurfaceControlBlur != null) {
                     mSurfaceControlBlur.show();
                 }
+
+                mService.updateNonSystemOverlayWindowsVisibilityIfNeeded(mWin, true);
                 if (mWin.mTurnOnScreen) {
                     if (DEBUG_VISIBILITY) Slog.v(TAG,
                             "Show surface turning screen on: " + mWin);
